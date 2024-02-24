eafec04714a18e598bd3a3bcf2a0bfea9505cf20
Edson Batista <edson.batista@brmalls.com.br>
2024-02-24 16:12:57 -0300

    create signUp page layout

--- src/pages/auth/SignIn/index.tsx
+++ src/pages/auth/SignIn/index.tsx
@@ -1,5 +1,7 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
 import * as z from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -23,12 +25,22 @@ export function SignIn() {
     console.log(data)
 
     await new Promise((resolve) => setTimeout(resolve, 2000))
+    toast.success('Enviamos um link de autenticação para o seu e-mail', {
+      action: {
+        label: 'Reenviar',
+        onClick: () => null,
+      },
+    })
   }
 
   return (
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
