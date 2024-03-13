5e295a0c4af3e67e38ca8697c5e903192f6fad3a
Edson Batista <edson.batista@brmalls.com.br>
2024-03-13 15:10:12 -0300

    add api delay and skeleton when data is loading

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,11 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true, // send cookies automatically
 })
+
+if (env.VITE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 3000))
+
+    return config
+  })
+}
