49b5b6bf152ea351a70fa509c81ceaf09cecc759
Edson Batista <edson.batista@brmalls.com.br>
2024-03-23 13:06:07 -0300

    get metrics amount from api

--- src/pages/app/Dashboard/day-orders-amount-card.tsx
+++ src/pages/app/Dashboard/day-orders-amount-card.tsx
@@ -1,8 +1,15 @@
+import { useQuery } from '@tanstack/react-query'
 import { Utensils } from 'lucide-react'
 
+import { getDayOrdersAmount } from '@/api/get-day-orders-amount'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export function DayOrdersAmountCard() {
+  const { data: daysOrdersAmount } = useQuery({
+    queryFn: getDayOrdersAmount,
+    queryKey: ['metrics', 'day-orders-amount'],
+  })
+
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -10,11 +17,30 @@ export function DayOrdersAmountCard() {
         <Utensils className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">2</span>
-        <p className="text-xs text-muted-foreground">
-          <span className="text-rose-500 dark:text-rose-400">-9%</span> em
-          relação a ontem
-        </p>
+        {daysOrdersAmount && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {daysOrdersAmount.amount.toLocaleString('pt-BR')}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              {daysOrdersAmount.diffFromYesterday >= 0 ? (
+                <>
+                  <span className="emerald-rose-500 dark:text-emerald-400">
+                    +{daysOrdersAmount.diffFromYesterday}%
+                  </span>{' '}
+                  em relação a ontem
+                </>
+              ) : (
+                <>
+                  <span className="text-rose-500 dark:text-rose-400">
+                    {daysOrdersAmount.diffFromYesterday}%
+                  </span>{' '}
+                  em relação a ontem
+                </>
+              )}
+            </p>
+          </>
+        )}
       </CardContent>
     </Card>
   )
--- src/pages/app/Dashboard/month-revenue-card.tsx
+++ src/pages/app/Dashboard/month-revenue-card.tsx
@@ -1,8 +1,15 @@
+import { useQuery } from '@tanstack/react-query'
 import { DollarSign } from 'lucide-react'
 
+import { getMonthRevenue } from '@/api/get-month-revenue'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export function MonthRevenueCard() {
+  const { data: monthRevenueAmount } = useQuery({
+    queryFn: getMonthRevenue,
+    queryKey: ['metrics', 'month-revenue-amount'],
+  })
+
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -12,11 +19,30 @@ export function MonthRevenueCard() {
         <DollarSign className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">R$ 1248,90</span>
-        <p className="text-xs text-muted-foreground">
-          <span className="text-emerald-500 dark:text-emerald-400">+5%</span> em
-          relação ao mês passado
-        </p>
+        {monthRevenueAmount && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {(monthRevenueAmount.receipt / 100).toLocaleString('pt-BR')}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              {monthRevenueAmount.diffFromLastMonth >= 0 ? (
+                <>
+                  <span className="emerald-rose-500 dark:text-emerald-400">
+                    +{monthRevenueAmount.diffFromLastMonth}%
+                  </span>{' '}
+                  em relação a mês passado
+                </>
+              ) : (
+                <>
+                  <span className="text-rose-500 dark:text-rose-400">
+                    {monthRevenueAmount.diffFromLastMonth}%
+                  </span>{' '}
+                  em relação a mês passado
+                </>
+              )}
+            </p>
+          </>
+        )}
       </CardContent>
     </Card>
   )
